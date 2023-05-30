8d08ab33a2194ee16e4257c1e798f735e5a1d9b8
showkhin1997 <showkhin15@gmail.com>
2023-05-30 09:46:22 +0600

    quantity and price update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function productQuantityPrice(isIncrease) {
+function productQuantityPrice(price, isIncrease) {
     const inputField = document.getElementById('laptop-quantity-input-field');
     let productQuantityValue = parseInt(inputField.value);
     if (isIncrease == true) {
@@ -9,13 +9,16 @@ function productQuantityPrice(isIncrease) {
             productQuantityValue = productQuantityValue - 1;
         }
     }
+    const productPrice = price * productQuantityValue;
+    console.log(productPrice);
+    document.getElementById('laptop-price').innerText = productPrice;
     inputField.value = productQuantityValue;
 }
 
 document.getElementById('laptop-quantity-add-button').addEventListener('click', function () {
-    productQuantityPrice(true);
+    productQuantityPrice(98000, true);
 });
 
 document.getElementById('laptop-quantity-minus-button').addEventListener('click', function () {
-    productQuantityPrice(false);
+    productQuantityPrice(98000, false);
 })
