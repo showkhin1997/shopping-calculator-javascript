be68ba42079101ec58f7da14cf0d15439765d80f
showkhin1997 <showkhin15@gmail.com>
2023-05-30 10:22:24 +0600

    calculate price

--- js/app.js
+++ js/app.js
@@ -9,11 +9,31 @@ function productQuantityPrice(product, price, isIncrease) {
             productQuantityValue = productQuantityValue - 1;
         }
     }
-    const productPrice = price * productQuantityValue;
-    console.log(productPrice);
-    document.getElementById(product + '-price').innerText = productPrice;
+
     inputField.value = productQuantityValue;
+
+    const productPrice = price * productQuantityValue;
+    document.getElementById(product + '-price').innerText = productPrice;
+    totalPrice();
 }
+
+function getProductQuantity(product) {
+    const inputField = document.getElementById(product + '-quantity-input-field');
+    const productQuantity = inputField.value;
+    return productQuantity;
+}
+
+function totalPrice() {
+    const laptopPrice = getProductQuantity('laptop') * 98000;
+    const mousePrice = getProductQuantity('mouse') * 1250;
+    const subTotal = laptopPrice + mousePrice;
+    const taxAmmount = subTotal * 0.1;
+    const totalPrice = subTotal + taxAmmount;
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax').innerText = taxAmmount;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
 
 document.getElementById('laptop-quantity-add-button').addEventListener('click', function () {
     productQuantityPrice('laptop', 98000, true);
