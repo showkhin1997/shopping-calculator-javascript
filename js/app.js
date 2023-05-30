31f00c8c5a7582c3f2f0440765a0932097182fd0
showkhin1997 <showkhin15@gmail.com>
2023-05-30 09:38:00 +0600

    quantity add and remove button work

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
-function productQuantityPrice() {
-    const inputField = document.getElementById('quantity-input-field');
+function productQuantityPrice(isIncrease) {
+    const inputField = document.getElementById('laptop-quantity-input-field');
     let productQuantityValue = parseInt(inputField.value);
-    productQuantityValue = productQuantityValue + 1;
+    if (isIncrease == true) {
+        productQuantityValue = productQuantityValue + 1;
+    }
+    else if (isIncrease == false) {
+        if (productQuantityValue > 0) {
+            productQuantityValue = productQuantityValue - 1;
+        }
+    }
     inputField.value = productQuantityValue;
 }
 
-document.getElementById('quantity-add-button').addEventListener('click', function () {
-    productQuantityPrice();
+document.getElementById('laptop-quantity-add-button').addEventListener('click', function () {
+    productQuantityPrice(true);
+});
+
+document.getElementById('laptop-quantity-minus-button').addEventListener('click', function () {
+    productQuantityPrice(false);
 })
